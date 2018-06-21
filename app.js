ef541518a5434b9cf99119a432126f82d669386e
DonBattery <mikloslorinczi.it@gmail.com>
2018-06-21 10:44:16 +0200

    /fizzbuzz/:num Endpoint added

--- app.js
+++ app.js
@@ -7,6 +7,21 @@ app.get('/', (req, res) =>  {
   res.send('Hi');
 });
 
+app.get('/fizzbuzz/:num', (req, res) => {
+  let response = '';
+  if (isNaN(req.params.num)) {
+    response = `${req.params.num} is not a number`
+  } else {
+    if (req.params.num % 3 == 0) {
+      response += 'fizz';
+    }
+    if (req.params.num % 5 == 0) {
+      response += 'buzz';
+    }
+  }
+  res.send(response);
+});
+
 module.exports = {
   app
 }
